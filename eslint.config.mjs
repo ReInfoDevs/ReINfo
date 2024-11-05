9fed2810a1524a24dc8e7cb252f191d701b47cfe
Samuel <samlarivs@outlook.com>
2024-11-05 10:27:07 -0500

    Fix: adds configuration for eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,34 +2,36 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
 
-/** @type {import('eslint').Linter.FlatConfig[]} */
-export default [
-  {
-    files: ["**/*.{js,mjs,cjs,jsx}"],
-    languageOptions: {
-      ecmaVersion: 2021,
-      sourceType: "module",
-      globals: globals.browser,
-    },
-    plugins: {
-      react: pluginReact,
-    },
-    settings: {
-      react: {
-        version: "detect",
+/** @type {import('eslint').Linter.Config} */
+export default {
+  overrides: [
+    {
+      files: ["**/*.{js,mjs,cjs,jsx}"],
+      languageOptions: {
+        ecmaVersion: 2021,
+        sourceType: "module",
+        globals: globals.browser,
+      },
+      plugins: {
+        react: pluginReact,
+      },
+      settings: {
+        react: {
+          version: "detect",
+        },
+      },
+      rules: {
+        ...pluginJs.configs.recommended.rules,
+        ...pluginReact.configs.recommended.rules,
       },
     },
-    rules: {
-      ...pluginJs.configs.recommended.rules,
-      ...pluginReact.configs.recommended.rules,
+    {
+      files: ["webpack.config.js"],
+      languageOptions: {
+        ecmaVersion: 2021,
+        sourceType: "module",
+        globals: globals.node, // Enable Node.js globals for `webpack.config.js`
+      },
     },
-  },
-  {
-    files: ["webpack.config.js"],
-    languageOptions: {
-      ecmaVersion: 2021,
-      sourceType: "module",
-      globals: globals.node, // Enable Node.js globals for `webpack.config.js`
-    },
-  },
-];
+  ],
+};
